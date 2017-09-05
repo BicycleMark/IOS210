3512d55528519859da8e048056729a430d074eac
Adam Patridge <patridge@gmail.com>
2017-09-05 11:40:22 -0600

    Initial course exercises import.

---
+++ Exercise 2/Completed/CancellableTaskViewController.designer.cs
@@ -0,0 +1,46 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace CancellableTasks
+{
+    [Register ("CancellableTaskViewController")]
+    partial class CancellableTaskViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnCalculate { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView txtPi { get; set; }
+
+        [Action ("HandleCalculatePi:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void HandleCalculatePi (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnCalculate != null) {
+                btnCalculate.Dispose ();
+                btnCalculate = null;
+            }
+
+            if (txtPi != null) {
+                txtPi.Dispose ();
+                txtPi = null;
+            }
+
+            if (txtPi != null) {
+                txtPi.Dispose ();
+                txtPi = null;
+            }
+        }
+    }
+}
